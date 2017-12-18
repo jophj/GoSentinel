ba451aa969207421ada956aea719364e020de24c
Giovanni Pinto <giovanni.pinto.po@gmail.com>
2017-12-18 20:51:58 +0100

    Added GymStateActionController

---
+++ GoSentinel.Tests/Bots/Controllers/BotAction/GymStateActionTests.cs
@@ -0,0 +1,14 @@
+﻿using GoSentinel.Bots.Controllers.BotAction;
+
+namespace GoSentinel.Tests.Bots.Controllers.BotAction
+{
+    public class GymStateActionTests
+    {
+        private readonly GymStateActionController _controller;
+
+        public GymStateActionTests()
+        {
+            _controller = new GymStateActionController();
+        }
+    }
+}
---
+++ GoSentinel/Bots/Controllers/BotAction/GymStateActionController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GoSentinel.Data;
+
+namespace GoSentinel.Bots.Controllers.BotAction
+{
+    public class GymStateActionController : IActionController<GymStateAction>
+    {
+        public IActionResponse Handle(IAction action)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
