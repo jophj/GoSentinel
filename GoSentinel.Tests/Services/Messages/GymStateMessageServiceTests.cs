633bbda13a7b4b201c637d3338e18eba9137c263
Giovanni Pinto <giovanni.pinto.po@gmail.com>
2018-02-13 14:14:31 +0100

    #14 Added GymStateMessageService

---
+++ GoSentinel.Tests/Services/Messages/GymStateMessageServiceTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Google.Protobuf.Collections;
+using GoSentinel.Data;
+using GoSentinel.Services.Messages;
+using POGOProtos.Data;
+using POGOProtos.Data.Gym;
+using POGOProtos.Data.Player;
+using POGOProtos.Enums;
+using Telegram.Bot.Types;
+using Xunit;
+using GymState = GoSentinel.Models.GymState;
+
+namespace GoSentinel.Tests.Services.Messages
+{
+    public class GymStateMessageServiceTests
+    {
+        private readonly GymStateMessageService _service;
+
+        public GymStateMessageServiceTests()
+        {
+            _service = new GymStateMessageService();
+        }
+
+        [Fact]
+        public void Generate_WithNullArgument_ShouldThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => _service.Generate(null));
+        }
+
+        [Fact]
+        public void Generate_WithNoGymState_ShouldThrowArgumentException()
+        {
+            var actionResponse = MakeActionResponse();
+            actionResponse.GymState = null;
+
+            Assert.Throws<ArgumentException>(() => _service.Generate(actionResponse));
+        }
+
+
+
+        protected GymStateActionResponse MakeActionResponse()
+        {
+            return new GymStateActionResponse()
+            {
+                GymState = new GymState()
+                {
+                    Id = "GymId",
+                    Name = "GymName",
+                    OwnedByTeam = TeamColor.Red,
+                    Memberships = new RepeatedField<GymMembership>()
+                    {
+                        new GymMembership()
+                        {
+                            PokemonData = new PokemonData()
+                            {
+                                Id = 666,
+                                PokemonId = PokemonId.Kingler,
+                                Cp = 2456,
+                                DisplayCp = 345
+                            },
+                            TrainerPublicProfile = new PlayerPublicProfile()
+                            {
+                                Name = "Ovit",
+                                TeamColor = TeamColor.Red
+                            }
+                        }
+                    }
+                },
+                Action = new GymStateAction()
+                {
+                    GymName = "GymName",
+                    Message = new Message()
+                    {
+                        Chat = new Chat()
+                        {
+                            Id = 0
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
--- GoSentinel/Services/Messages/GymStateMessageService.cs
+++ GoSentinel/Services/Messages/GymStateMessageService.cs
@@ -1,4 +1,5 @@
-﻿using GoSentinel.Data;
+﻿using System;
+using GoSentinel.Data;
 
 namespace GoSentinel.Services.Messages
 {
@@ -6,6 +7,16 @@ namespace GoSentinel.Services.Messages
     {
         public string Generate(GymStateActionResponse actionResponse)
         {
+            if (actionResponse == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (actionResponse.GymState == null)
+            {
+                throw new ArgumentException("Gym state should not be null");
+            }
+
             return null;
         }
     }
